3b2f2f243af5ace7b88f9641649420f16f644765
dmitry <tsmyh@mail.ru>
2014-12-08 11:36:23 +0300

    modified base and local

--- heroku_project/settings/local.py
+++ heroku_project/settings/local.py
@@ -84,7 +84,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static-only')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
