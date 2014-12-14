13bdb0f3c2f509cec427d959debef5037561d12a
dmitry <tsmyh@mail.ru>
2014-12-14 10:34:15 +0300

    delete cach

--- heroku_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-
-urlpatterns = patterns('',
-	url(r'^$','heroku_app.views.home', name='home'),
-	url(r'^(?P<ref_id>.+)/$','heroku_app.views.share', name='share'),
-	) 
